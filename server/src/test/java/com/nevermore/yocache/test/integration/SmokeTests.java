f3e96a27bdb0028903cc5ac129d72b9ed9cd96a9
Sun Cheng <406504325@qq.com>
2025-01-07 19:37:26 +0800

    fix bug

--- server/src/test/java/com/nevermore/yocache/test/integration/SmokeTests.java
+++ server/src/test/java/com/nevermore/yocache/test/integration/SmokeTests.java
@@ -1,42 +1,44 @@
 package com.nevermore.yocache.test.integration;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.service.annotation.GetExchange;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SmokeTests {
-
-    @Value("${local.server.port}")
-    private int port;
-
     private final RestClient client;
 
-    public SmokeTests() {
+    public SmokeTests(@LocalServerPort int port) {
         client = RestClient.create("http://localhost:" + port);
     }
 
     @Test
+    @Order(1)
     public void testPut() {
-        System.out.println(port);
-        client.put().uri("/test/a/b").body(new byte[]{0})
+        client.put().uri("/test/a/b").body(new byte[] { 0 })
                 .retrieve()
                 .onStatus(status -> !status.is2xxSuccessful(), (req, res) -> {
                     throw new RuntimeException(res.getStatusText());
-                });
+                }).toBodilessEntity();
     }
 
     @Test
-    @GetExchange
+    @Order(2)
     public void testGet() {
+        long start = System.currentTimeMillis();
         client.get().uri("/test/a/b")
                 .retrieve()
                 .onStatus(status -> !status.is2xxSuccessful(), (req, res) -> {
                     throw new RuntimeException(res.getStatusText());
-                });
+                }).toEntity(byte[].class);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
     }
 }
